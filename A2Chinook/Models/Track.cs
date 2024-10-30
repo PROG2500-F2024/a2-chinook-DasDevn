de58935ce77b1ec736af88a3701b01e1380ec3f5
Devan Eisnor <W0196059@nscc.ca>
2024-10-30 19:17:39 -0300

    Two Partial Classes Created in Tracks

---
+++ A2Chinook/Models/Track.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A2Chinook.Models
+{
+    public partial class Track
+    {
+        public string FormattedTime
+        {
+            get
+            {
+                int totalSeconds = Milliseconds / 1000;
+                int minutes = totalSeconds / 60;
+                int seconds = totalSeconds % 60;
+                return $"{minutes}:{seconds:D2} "; 
+            }
+        }
+
+        public string SizeInMB
+        {
+            get
+            {
+                if (Bytes.HasValue)
+                {
+                    double megabytes = Bytes.Value / (1024.0 * 1024.0); // 
+                    return $"{megabytes:F2} MB";
+                }
+                else
+                {
+                    return "N/A";
+                }
+            }
+        }
+    }
+}
