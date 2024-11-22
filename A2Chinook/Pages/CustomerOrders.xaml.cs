8f8ef1056ba16b679b9fb03a3c8900b73bb67b85
Devan Eisnor <W0196059@nscc.ca>
2024-11-22 12:39:43 -0400

    Orders working

--- A2Chinook/Pages/CustomerOrders.xaml.cs
+++ A2Chinook/Pages/CustomerOrders.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using A2Chinook.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +22,52 @@ namespace A2Chinook.Pages
     /// </summary>
     public partial class CustomerOrders : Page
     {
+        private readonly ChinookContext _context = new ChinookContext();
+        private CollectionViewSource customerOrdersViewSource;
+
+
         public CustomerOrders()
         {
             InitializeComponent();
+
+            customerOrdersViewSource = (CollectionViewSource)FindResource(nameof(customerOrdersViewSource));
+
+            //Load data from all sets
+            _context.Customers.Load();
+            _context.InvoiceLines.Load();
+            _context.Invoices.Load();
+
+            customerOrdersViewSource.Source = _context.Customers.Local.ToObservableCollection();
+        }
+
+        private void btnSearch_Click(object sender, RoutedEventArgs e)
+        {
+            var query =
+                from cust in _context.Customers
+                where cust.LastName.Contains(textSearch.Text)
+                select new
+                {
+                    FullName = cust.FirstName + " " + cust.LastName,
+                    cust.City,
+                    cust.Country,
+                    cust.Email,
+                    Invoices = (
+                        from inv in cust.Invoices
+                        orderby inv.InvoiceDate descending
+                        select new
+                        {
+                            inv.InvoiceDate,
+                            inv.Total,
+                            TotalQuantity = (
+                                from line in inv.InvoiceLines
+                                select line.Quantity
+                            ).Sum()
+                        }
+                    ).ToList()
+                };
+
+            catalogListView.ItemsSource = query.ToList();
         }
     }
 }
+
