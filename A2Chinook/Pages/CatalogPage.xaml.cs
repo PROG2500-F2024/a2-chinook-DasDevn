96ee6a5524c3e60d13666fd0ac4b58eb8bdc7b84
Devan Eisnor <W0196059@nscc.ca>
2024-11-21 21:51:51 -0400

    Count working

--- A2Chinook/Pages/CatalogPage.xaml.cs
+++ A2Chinook/Pages/CatalogPage.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using A2Chinook.Data;
+using A2Chinook.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +23,38 @@ namespace A2Chinook.Pages
     /// </summary>
     public partial class CatalogPage : Page
     {
+        private readonly ChinookContext _context = new ChinookContext();
+        private CollectionViewSource catalogViewSource;
+
         public CatalogPage()
         {
             InitializeComponent();
+            catalogViewSource = (CollectionViewSource)FindResource(nameof(catalogViewSource));
+
+            //Load Data from BOTH sets
+            _context.Artists.Load();
+            _context.Albums.Load();
+
+            catalogViewSource.Source = _context.Artists.Local.ToObservableCollection();
+        }
+
+        private void btnSearch_Click(object sender, RoutedEventArgs e)
+        {
+            //Define a grouping query to get grouped category data
+            var query =
+                from artist in _context.Artists
+                where artist.Name.Contains(textSearch.Text)
+                group artist by artist.Name.ToUpper().Substring(0, 1) into artistGroup 
+                select new
+                {
+                    Index = artistGroup.Key,
+                    ArtistCount = artistGroup.Count().ToString(),
+                    artist = artistGroup.ToList<Artist>()
+                    
+                };
+
+            // Execute query and bind to the ListView
+            catalogListView.ItemsSource = query.ToList();
         }
     }
 }
